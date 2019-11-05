b06a3f49440cc4ec92bc92815716e071b07b4089
Notesong <notesong.git@outlook.com>
2019-11-05 01:44:44 -0500

    Add menu functionality. MVP complete

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -10,7 +10,6 @@ let menuItems = [
 ];
 
 /* 
-
   Step 1: Write a function that will create a menu component as seen below:
 
   <div class="menu">
@@ -23,13 +22,38 @@ let menuItems = [
 
   Step 2: Inside this function, iterate over the array creating a list item <li> element for each item in the array. 
   Add those items to the <ul>
-
   Step 3: Using a DOM selector, select the menu button (the element with a class of 'menu-button') currently on the DOM.
-
   Step 4: add a click event listener to the menu button. When clicked it should toggle the class 'menu--open' on the menu (your div with a 'menu' class).
-
   Step 5: return the menu component.
-
   Step 6: add the menu component to the DOM.
   
 */
+
+function createMenu(arr) {
+  // main menu div setup
+  const menu = document.createElement('div');
+  menu.classList.add('menu');
+
+  // unordered list 'ul' setup
+  const unorderedList = document.createElement('ul');
+  menu.appendChild(unorderedList);
+
+  // create all list items from array
+  arr.forEach(item => {
+    const listItem = document.createElement('li');
+    unorderedList.appendChild(listItem);
+    listItem.textContent = item;
+  });
+
+  // menu functionality
+  const menuButton = document.querySelector('.menu-button');
+  menuButton.addEventListener('click', () => {
+    menu.classList.toggle('menu--open');
+  });
+
+  return menu;
+}
+
+const menuParentComponent = document.querySelector('.header');
+const newMenu = createMenu(menuItems);
+menuParentComponent.prepend(newMenu);
